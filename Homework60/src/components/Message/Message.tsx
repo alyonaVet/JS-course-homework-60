72cd3441989966a5f0e2d0381e7122b2e5e6c793
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-17 20:42:28 +0600

    created component Message;

---
+++ Homework60/src/components/Message/Message.tsx
@@ -0,0 +1,31 @@
+import {Box, Typography} from '@mui/material';
+import React from 'react';
+
+interface Props {
+  message: string;
+  author: string;
+  datetime: string;
+}
+
+const Message: React.FC<Props> = ({message, author, datetime}) => {
+  return (
+    <Box component="section" sx={{
+      border: '1px solid grey',
+      borderRadius: '8px',
+      padding: '16px',
+      width: '660px',
+    }}>
+      <Typography variant="h6" component="p">
+        {author}
+      </Typography>
+      <Typography variant="body2" component="p">
+        {message}
+      </Typography>
+      <Typography variant="body2" color="textSecondary" sx={{ textAlign: 'right' }}>
+        {datetime}
+      </Typography>
+    </Box>
+  );
+};
+
+export default Message;
