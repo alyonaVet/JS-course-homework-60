939b8f2e9bab1522c183163346d68a6eb9f6d091
alyonaVet <a.v.alenka2015@gmail.com>
2024-06-18 00:59:51 +0600

    refactor and added second useEffect;

--- Homework60/src/containers/Chat/Chat.tsx
+++ Homework60/src/containers/Chat/Chat.tsx
@@ -4,31 +4,51 @@ import AddMessageForm from '../../components/AddMessageForm/AddMessageForm';
 import {Stack} from '@mui/material';
 import Message from '../../components/Message/Message';
 
-const url = 'http://146.185.154.90:8000/messages';
 
 const Chat = () => {
   const [messages, setMessages] = useState<MessageType[]>([]);
 
+  const getMessages = async (lastMessage?: MessageType) => {
+
+    console.log(lastMessage);
+
+    const suffix = !lastMessage ? '' : `?datetime=${lastMessage.datetime}`;
+
+    const url = `http://146.185.154.90:8000/messages${suffix}`;
+
+    const response = await fetch(url);
+    if (response.ok) {
+      const messagesData = await response.json() as MessageType[];
+      const newMessages = messagesData.map((message) => ({
+        _id: message._id,
+        message: message.message,
+        author: message.author,
+        datetime: message.datetime,
+      }));
+
+      setMessages([...messages, ...newMessages]);
+    }
+  };
+
   useEffect(() => {
-    const getMessages = async () => {
-      const response = await fetch(url);
-      if (response.ok) {
-        const messagesData = await response.json() as MessageType[];
-        const newMessages = messagesData.map((message) => ({
-          _id: message._id,
-          message: message.message,
-          author: message.author,
-          datetime: message.datetime,
-        }));
-        setMessages(newMessages);
-      }
-    };
+
     void getMessages();
+
   }, []);
+
+  useEffect(() => {
+
+    const lastMessage: MessageType = messages[messages.length - 1];
+
+    const interval = setInterval(() => getMessages(lastMessage), 5000);
+
+    return () => clearInterval(interval);
+
+  }, [messages]);
 
   return (
     <>
-      <AddMessageForm />
+      <AddMessageForm/>
       <Stack spacing={2} sx={{minWidth: 12}} direction="column" alignItems="center" mt={5}>
         {messages.map((message: MessageType) => {
           return <Message
